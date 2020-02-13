1abd4794e162ed140235a1d82a229fd117223452
Steven <NightAngel47@users.noreply.github.com>
2020-02-12 18:42:49 -0600

    Customer

--- Normal Pizza for Normal People/Assets/Scripts/CustomerTest.cs
+++ Normal Pizza for Normal People/Assets/Scripts/CustomerTest.cs
@@ -1,48 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class CustomerTest : MonoBehaviour
 {
-    List<GameObject> ingredients = new List<GameObject>();
+    private List<GameObject> orderIngredients = new List<GameObject>();
 
-    public GameObject topping1;
+    public GameObject testTopping;
 
     public void Start()
     {
-        ingredients.Add(topping1);
+        orderIngredients.Add(testTopping);
     }
 
     private void OnTriggerEnter(Collider col)
     {
-        if(ingredients.Count == col.gameObject.GetComponent<PizzaScript>().ingredientList.Count)
+        // is ths collider a pizza
+        if (!col.TryGetComponent(out PizzaScript pizza)) return;
+
+        if (IsOrderCorrect(pizza))
         {
-            foreach(GameObject g in ingredients)
-            {
-                foreach(GameObject k in col.gameObject.GetComponent<PizzaScript>().ingredientList)
-                {
-                    if(g.name == k.name)
-                    {
-                        ingredients.Remove(g);
-                        col.gameObject.GetComponent<PizzaScript>().ingredientList.Remove(k);
-                    }
-                }
-            }
-
-            if(ingredients.Count == col.gameObject.GetComponent<PizzaScript>().ingredientList.Count) //both should be empty if done right
-            {
-                //pizza delivered right
-            }
-
-            else
-            {
-                //bad pizza
-            }
+            //TODO order and pizza match
         }
-
         else
         {
-            //not same size not same pizza
+            //TODO order and pizza don't match
         }
+    }
+
+    /// <summary>
+    /// Checks if ingredients on pizza are the same as ingredients in order
+    /// </summary>
+    /// <param name="pizza">The pizza being checked.</param>
+    /// <returns>Returns true if pizza and order match. Returns false if they don't.</returns>
+    private bool IsOrderCorrect(PizzaScript pizza)
+    {
+        List<GameObject> tempPizzaIngredients = pizza.ingredientList;
+        List<GameObject> tempOrderIngredients = orderIngredients;
+        
+        foreach (var orderIngredient in tempOrderIngredients)
+        {
+            foreach (var pizzaIngredient in tempPizzaIngredients.Where(pizzaIngredient => orderIngredient.name == pizzaIngredient.name))
+            {
+                tempOrderIngredients.Remove(orderIngredient);
+                tempPizzaIngredients.Remove(pizzaIngredient);
+            }
+        }
+
+        return tempOrderIngredients.Count <= 0 && tempPizzaIngredients.Count <= 0;
     }
 }
