20a2ecde86305d1babd81035897ec4a42709103e
Steven <NightAngel47@users.noreply.github.com>
2020-02-18 14:27:04 -0600

    Interactable

--- Normal Pizza for Normal People/Assets/Scripts/Interactable Objects/InteractableHover.cs
+++ Normal Pizza for Normal People/Assets/Scripts/Interactable Objects/InteractableHover.cs
@@ -3,41 +3,38 @@ using UnityEngine.Events;
 using System.Collections;
 using Valve.VR.InteractionSystem;
 
+[RequireComponent(typeof(Interactable1))]
 public class InteractableHover : MonoBehaviour
 {
-	[RequireComponent(typeof(Interactable1))]
-	public class InteractableHoverEvents : MonoBehaviour
+	public UnityEvent onHandHoverBegin;
+	public UnityEvent onHandHoverEnd;
+	public UnityEvent onAttachedToHand;
+	public UnityEvent onDetachedFromHand;
+
+	//-------------------------------------------------
+	private void OnHandHoverBegin()
 	{
-		public UnityEvent onHandHoverBegin;
-		public UnityEvent onHandHoverEnd;
-		public UnityEvent onAttachedToHand;
-		public UnityEvent onDetachedFromHand;
-
-		//-------------------------------------------------
-		private void OnHandHoverBegin()
-		{
-			onHandHoverBegin.Invoke();
-		}
+		onHandHoverBegin.Invoke();
+	}
 
 
-		//-------------------------------------------------
-		private void OnHandHoverEnd()
-		{
-			onHandHoverEnd.Invoke();
-		}
+	//-------------------------------------------------
+	private void OnHandHoverEnd()
+	{
+		onHandHoverEnd.Invoke();
+	}
 
 
-		//-------------------------------------------------
-		private void OnAttachedToHand(Hand hand)
-		{
-			onAttachedToHand.Invoke();
-		}
+	//-------------------------------------------------
+	private void OnAttachedToHand(Hand hand)
+	{
+		onAttachedToHand.Invoke();
+	}
 
 
-		//-------------------------------------------------
-		private void OnDetachedFromHand(Hand hand)
-		{
-			onDetachedFromHand.Invoke();
-		}
+	//-------------------------------------------------
+	private void OnDetachedFromHand(Hand hand)
+	{
+		onDetachedFromHand.Invoke();
 	}
 }
