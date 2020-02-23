01aea4d03d0f8d04f56b3c0c290d0fe76775f970
sidfoe <sidfoe2125@gmail.com>
2020-02-23 17:12:26 -0600

    pointer baby

---
+++ Normal Pizza for Normal People/Assets/Scripts/Pointer.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pointer : MonoBehaviour
+{
+    public float defaultLength = 5;
+    public GameObject dot;
+    public VRInputModule im;
+
+    private LineRenderer lr = null; 
+
+    private void Awake()
+    {
+        lr = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        UpdateLine();
+    }
+
+    private void UpdateLine()
+    {
+        //use default of distance 
+        float targetLength = defaultLength;
+
+        //raycast
+        RaycastHit hit = CreateRaycast(targetLength);
+
+        //default 
+        Vector3 endPos = transform.position + (transform.forward * targetLength);
+
+        //or based on hit
+        if(hit.collider !=null)
+        {
+            endPos = hit.point;
+        }
+
+        //set pos of dot
+        dot.transform.position = endPos;
+
+        //set line renderer
+        lr.SetPosition(0, transform.position);
+        lr.SetPosition(1, endPos);
+    }
+
+    private RaycastHit CreateRaycast(float length)
+    {
+        RaycastHit hit;
+
+        Ray ray = new Ray(transform.position, transform.forward);
+        Physics.Raycast(ray, out hit, defaultLength);
+
+        return hit;
+    }
+}
