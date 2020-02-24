6f609a8ec47f7f458b64cf9001beea0705d84d4b
sidfoe <sidfoe2125@gmail.com>
2020-02-23 18:41:20 -0600

    the beginings of the oven

--- Normal Pizza for Normal People/Assets/Scripts/OvenBehaviour.cs
+++ Normal Pizza for Normal People/Assets/Scripts/OvenBehaviour.cs
@@ -1,24 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
 
 public class OvenBehaviour : MonoBehaviour
 {
     //private GameManager gm;
-    
+
     private bool inOven = false;
     public float cookTime = 10;
     public float overCookTime = 20;
-    private float currentTime = 0;
     public Material cooked;
     public Material burnt;
     public Image loadingBar;
     public TextMeshProUGUI progressIndicator;
 
     private float tempTime;
-    private bool overCooking = false;
+    private bool overCooking = false; //is it about to be burnt
+
+    public PizzaBehaviour pizza;
 
     private Color goodGreen = new Color(0.3f, 0.83f, 0.26f);
     private Color warnOrange = new Color(1f, 0.63f, 0f);
@@ -27,17 +28,22 @@ public class OvenBehaviour : MonoBehaviour
     //private AudioSource audioSource;
     //[SerializeField]
     //private AudioClip[] audioClips; // 0 = cooking, 1 = ding, 2 = burnt 3 = first placed
+    // Start is called before the first frame update
 
-    private void Start()
+    void Start()
     {
-        //gm = FindObjectOfType<GameManager>();
-        //ps = gameObject.GetComponent<ParticleSystem>();
-        //audioSource = GetComponent<AudioSource>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 
     private void OnTriggerEnter(Collider col)
     {
-        if (col.gameObject.CompareTag("pizza") && col.gameObject.GetComponentInParent<PizzaScript>().isBurnt == false)//&& !gm.isPaused
+        if (col.gameObject.GetComponentInParent<PizzaBehaviour>() == true && col.gameObject.GetComponentInParent<PizzaScript>().isBurnt == false)//&& !gm.isPaused
         {
             //audioSource.Stop();
             //audioSource.clip = audioClips[3];
@@ -45,20 +51,19 @@ public class OvenBehaviour : MonoBehaviour
             //audioSource.Play();
 
             //ps.Play();
-            currentTime = 0;
+            //currentTime = 0;
         }
     }
 
     private void OnTriggerStay(Collider col)
     {
-        //Debug.Log(col.tag);
-        if (col.gameObject.CompareTag("pizza") && col.gameObject.GetComponentInParent<PizzaScript>().isBurnt == false)// && !gm.isPaused
-        { 
-            inOven = true;
-            currentTime += Time.deltaTime;
-            int percent = ((int)currentTime / (int)cookTime) * 100;
+        //is the pizza in the oven and is it not burnt yet
+        if (col.gameObject.GetComponentInParent<PizzaBehaviour>() == true && col.gameObject.GetComponentInParent<PizzaScript>().isBurnt == false)// && !gm.isPaused
+        {
+            pizza.cookedTime += Time.deltaTime;
+            int percent = ((int)pizza.cookedTime / (int)cookTime) * 100;
             string per = (percent).ToString();
-            loadingBar.fillAmount = currentTime / cookTime;
+            loadingBar.fillAmount = pizza.cookedTime / cookTime;
 
             //if (!audioSource.isPlaying)
             //{
@@ -67,32 +72,34 @@ public class OvenBehaviour : MonoBehaviour
             //    audioSource.Play();
             //}
 
-            if (overCooking == true && currentTime >= cookTime)
+            //if it finished cooking this checks to see if it is going to burn/make it burnt
+            if (overCooking == true && pizza.cookedTime >= cookTime)
             {
                 col.gameObject.GetComponentInChildren<MeshRenderer>().material = burnt;
                 col.gameObject.GetComponentInParent<PizzaScript>().isCooked = false;
                 col.gameObject.GetComponentInParent<PizzaScript>().isBurnt = true;
                 loadingBar.color = warnOrange;
                 progressIndicator.text = "Burnt";
-                
+
                 //audioSource.Stop();
                 //audioSource.clip = audioClips[1];
                 //audioSource.loop = false;
                 //audioSource.Play();
-                
+
                 //audioSource.clip = audioClips[2];
                 //audioSource.Play();
             }
 
-            else if (currentTime >= cookTime)
+            //else it is not fully cooked yet and it checks to see if it is going to be cooked
+            else if (pizza.cookedTime >= cookTime)
             {
                 col.gameObject.GetComponentInChildren<MeshRenderer>().material = cooked;
                 col.gameObject.GetComponentInParent<PizzaScript>().isCooked = true;
-                currentTime = 0;
+                pizza.cookedTime = 0;
                 overCooking = true;
                 loadingBar.color = warnOrange;
                 progressIndicator.text = "Ready";
-                
+
                 //audioSource.Stop();
                 //audioSource.clip = audioClips[1];
                 //audioSource.loop = false;
@@ -103,13 +110,14 @@ public class OvenBehaviour : MonoBehaviour
 
     private void OnTriggerExit(Collider col)
     {
-        if (col.gameObject.CompareTag("pizza"))//&& !gm.isPaused
+        //if the pizza is removed reset all variables
+        if (col.gameObject.GetComponentInParent<PizzaBehaviour>() == true)//&& !gm.isPaused
         {
             //ps.Stop();
             //audioSource.Stop();
             inOven = false;
             overCooking = false;
-            currentTime = 0;
+            //currentTime = 0;
             loadingBar.color = goodGreen;
             loadingBar.fillAmount = 0;
             progressIndicator.text = "Cooking";
--- Normal Pizza for Normal People/Assets/Scripts/PizzaBehaviour.cs
+++ Normal Pizza for Normal People/Assets/Scripts/PizzaBehaviour.cs
@@ -6,6 +6,11 @@ public class PizzaBehaviour : MonoBehaviour
 {
     private List<PizzaIngredient> ingredientsOnPizza = new List<PizzaIngredient>();
 
+    //Oven/Cooking Variables
+    public float cookedTime = 0; //the amount of time the pizza has spent in the oven
+    public bool isBurnt = false;
+    public bool isCooked = false;
+
     public void AddPizzaIngredient(PizzaIngredient newIngredient)
     {
         ingredientsOnPizza.Add(newIngredient);
