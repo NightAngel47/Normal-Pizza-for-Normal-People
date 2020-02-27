cc9732845385842e0b3f946d0e9adc456c4afd5a
sidfoe <sidfoe2125@gmail.com>
2020-02-27 14:30:50 -0600

    oven

--- Normal Pizza for Normal People/Assets/Scripts/Pizza/PizzaBehaviour.cs
+++ Normal Pizza for Normal People/Assets/Scripts/Pizza/PizzaBehaviour.cs
@@ -7,7 +7,9 @@ public class PizzaBehaviour : MonoBehaviour
     private List<PizzaIngredient> ingredientsOnPizza = new List<PizzaIngredient>();
 
     //Oven/Cooking Variables
+    [HideInInspector]
     public float cookedTime = 0; //the amount of time the pizza has spent in the oven
+    public float counterTime = -1;
     public bool isBurnt = false; //has the pizza been burnt
     public bool isCooked = false; //has the pizza been cooked ----- only one of these two bools will be on at any given time
     public bool overCooking = false; //is it being over cooked, has it been cooked and will it be burning soon
