cbaf8d26b85b0c1e5450bc115f6ac139591c2394
Steven <NightAngel47@users.noreply.github.com>
2020-03-07 20:10:43 -0600

    Days and Timers

--- Normal Pizza for Normal People/Assets/Scripts/GameManager.cs
+++ Normal Pizza for Normal People/Assets/Scripts/GameManager.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(CustomerLine), typeof(OrderCreation))]
 public class GameManager : MonoBehaviour
@@ -9,11 +11,35 @@ public class GameManager : MonoBehaviour
     private CustomerLine customerLine;
     private MoneyTracker moneyTracker;
 
+    [SerializeField]
+    private TMP_Text currentDayText;
+    [SerializeField]
+    private TMP_Text currentDayTime;
+    [SerializeField]
+    private Image currentDayProgressBar;
+    
+    [SerializeField]
+    public List<Day> gameDays = new List<Day>(7);
+    private int currentDay = 0;
+    private float currentDayTimer;
+    
+
+    [Serializable]
+    public struct Day
+    {
+        [Header("Day Info")]
+        [Tooltip("The number of customers per day"), Range(0, 50)]
+        public int numOfCustomers;
+        [Tooltip("The length of each day in seconds"), Range(0, 300)]
+        public int dayLength;
+    }
+
     private void Start()
     {
         moneyTracker = new MoneyTracker();
         customerLine = GetComponent<CustomerLine>();
-        WorkDay();
+        //TODO have player start day
+        StartCoroutine(DayCycle());
     }
 
     public MoneyTracker GetMoneyTracker()
@@ -21,8 +47,29 @@ public class GameManager : MonoBehaviour
         return moneyTracker;
     }
 
-    private void WorkDay()
+    private IEnumerator DayCycle()
     {
-        customerLine.StartDay(10);
+        currentDayText.text = "Day " + currentDay + 1;
+        customerLine.StartDay(gameDays[currentDay].numOfCustomers);
+        currentDayTimer = gameDays[currentDay].dayLength;
+        StartCoroutine(DayTimer());
+        
+        //TODO add upgrade pause
+        
+        yield return new WaitForSeconds(gameDays[currentDay].dayLength);
+        currentDay++;
+        StartCoroutine(DayCycle());
+    }
+
+    private IEnumerator DayTimer()
+    {
+        currentDayTime.text = "" + (int) currentDayTimer;
+        currentDayProgressBar.fillAmount = currentDayTimer / gameDays[currentDay].dayLength;
+        yield return new WaitForEndOfFrame();
+        currentDayTimer -= Time.deltaTime;
+        if (currentDayTimer > 0)
+        {
+            StartCoroutine(DayTimer());
+        }
     }
 }
