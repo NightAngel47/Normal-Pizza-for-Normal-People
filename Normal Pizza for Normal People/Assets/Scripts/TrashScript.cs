ff2ba7ab7cf218369dd7cd16e61ef4c0e834e92f
sidfoe <sidfoe2125@gmail.com>
2020-03-09 22:18:48 -0500

    trash

---
+++ Normal Pizza for Normal People/Assets/Scripts/TrashScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashScript : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.transform.parent.TryGetComponent(out IngredientHitEffect ingredient) || col.transform.parent.TryGetComponent(out PizzaBehaviour pizza))
+        {
+            Destroy(col);
+        }
+    }
+}
