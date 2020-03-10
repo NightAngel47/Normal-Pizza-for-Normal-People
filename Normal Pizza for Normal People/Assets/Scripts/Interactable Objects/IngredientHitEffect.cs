2193c9a33abdb27fb366fa9a5a12d7255810d711
Steven <NightAngel47@users.noreply.github.com>
2020-03-09 22:31:11 -0500

    Update IngredientHitEffect.cs

--- Normal Pizza for Normal People/Assets/Scripts/Interactable Objects/IngredientHitEffect.cs
+++ Normal Pizza for Normal People/Assets/Scripts/Interactable Objects/IngredientHitEffect.cs
@@ -11,23 +11,21 @@ public class IngredientHitEffect : MonoBehaviour
     {
         var pizza = collision.collider.GetComponentInParent<PizzaBehaviour>();
 
-        if (pizza)
+        if (!pizza) return;
+        ContactPoint contact = collision.contacts[0];
+
+        float backTrackLength = 1f;
+        Ray ray = new Ray(contact.point - (-contact.normal * backTrackLength), -contact.normal);
+        if (collision.collider.Raycast(ray, out RaycastHit hit, 2))
         {
-            ContactPoint contact = collision.contacts[0];
-
-            float backTrackLength = 1f;
-            Ray ray = new Ray(contact.point - (-contact.normal * backTrackLength), -contact.normal);
-            if (collision.collider.Raycast(ray, out RaycastHit hit, 2))
-            {
-                var pizzaTransform = pizza.transform;
-                var spawnPos = pizzaTransform.position;
-                var randRot = pizzaTransform.rotation.eulerAngles;
-                randRot += new Vector3(0, Random.Range(-180f, 180f), 0);
-                var newIngredient = Instantiate(spawnObjectOnCollision, spawnPos, Quaternion.Euler(randRot), pizza.transform);
-                pizza.AddPizzaIngredient(newIngredient.GetComponent<PizzaIngredient>());
-            }
-
-            Destroy(gameObject);
+            var pizzaTransform = pizza.transform;
+            var spawnPos = pizzaTransform.position;
+            var randRot = pizzaTransform.rotation.eulerAngles;
+            randRot += new Vector3(0, Random.Range(-180f, 180f) - randRot.y, 0);
+            var newIngredient = Instantiate(spawnObjectOnCollision, spawnPos, Quaternion.Euler(randRot), pizza.transform);
+            pizza.AddPizzaIngredient(newIngredient.GetComponent<PizzaIngredient>());
         }
+
+        Destroy(gameObject);
     }
 }
