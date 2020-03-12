f55ee0bd6dcff12e7c6e761e2ca7cc17c867e027
Steven <NightAngel47@users.noreply.github.com>
2020-03-12 04:00:36 -0500

    Oven Sounds

--- Normal Pizza for Normal People/Assets/Scripts/OvenBehaviour.cs
+++ Normal Pizza for Normal People/Assets/Scripts/OvenBehaviour.cs
@@ -6,155 +6,113 @@ using UnityEngine.UI;
 
 public class OvenBehaviour : MonoBehaviour
 {
-    //private GameManager gm;
-
-    //private bool inOven = false;
-    public float cookTime = 10; //time till its cooked
-    public float overCookTime = 20; // time till its burnt
-    public Material cooked;
-    public Material burnt;
+    [SerializeField] private float cookTime = 10; //time till its cooked
+    [SerializeField] private float overCookTime = 20; // time till its burnt
+    [SerializeField] private Material cooked;
+    [SerializeField] private Material burnt;
 
     public Image loadingBar;
     public TextMeshProUGUI progressIndicator;
     private float timerTime;
 
-    private PizzaBehaviour pizza;
+    //private PizzaBehaviour pizza;
     private float tempTime = 0;
 
     private bool changeOnce = false;
 
+    [SerializeField]
     private Color goodGreen = new Color(0.3f, 0.83f, 0.26f);
+    [SerializeField]
     private Color warnOrange = new Color(1f, 0.63f, 0f);
 
     private ParticleSystem ps;
-
-    //private ParticleSystem ps;
-    //private AudioSource audioSource;
-    //[SerializeField]
-    //private AudioClip[] audioClips; // 0 = cooking, 1 = ding, 2 = burnt 3 = first placed
-    // Start is called before the first frame update
+    
+    private enum OvenAudioStates {Cooking, PizzaDone, PizzaBurnt}
+    [SerializeField] private List<AudioClip> ovenAudioClips = new List<AudioClip>();
+    private AudioSource audioSource;
 
     void Start()
     {
-        ps = GetComponent<ParticleSystem>();
+        ps = transform.parent.GetComponentInChildren<ParticleSystem>();
+        audioSource = GetComponent<AudioSource>();
         timerTime = cookTime + 1;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     private void OnTriggerEnter(Collider col)
     {
-        //if (col.gameObject.GetComponentInParent<PizzaBehaviour>() == true && col.gameObject.GetComponentInParent<PizzaScript>().isBurnt == false)//&& !gm.isPaused
-        //{
-        //    //audioSource.Stop();
-        //    //audioSource.clip = audioClips[3];
-        //    //audioSource.loop = false;
-        //    //audioSource.Play();
+        if (!col.transform.parent.TryGetComponent(out PizzaBehaviour pizza) || pizza.isBurnt) return;
+        
+        ps.Play();
+        PlayOvenAudio(OvenAudioStates.Cooking);
 
-        //    //ps.Play();
-        //    //currentTime = 0;
-        //}
-
-        if (col.gameObject.GetComponentInParent<PizzaBehaviour>() == true && col.gameObject.GetComponentInParent<PizzaBehaviour>().isBurnt == false)
+        if(pizza.isCooked)
         {
-            //ps.Play();
-
-            if(col.gameObject.GetComponentInParent<PizzaBehaviour>().isCooked == true)
-            {
-                tempTime = col.gameObject.GetComponentInParent<PizzaBehaviour>().cookedTime - cookTime;
-            }
-
-            else
-            {
-                tempTime = col.gameObject.GetComponentInParent<PizzaBehaviour>().cookedTime;
-            }
+            tempTime = pizza.cookedTime - cookTime;
+        }
+        else
+        {
+            tempTime = pizza.cookedTime;
+        }
             
-            if(col.gameObject.GetComponentInParent<PizzaBehaviour>().counterTime == -1)
-            {
-                Debug.Log("here3");
-                timerTime = cookTime + 1;
-            }
-
-            else
-            {
-                timerTime = col.gameObject.GetComponentInParent<PizzaBehaviour>().counterTime;
-            }
+        if(pizza.counterTime == -1)
+        {
+            timerTime = cookTime + 1;
+        }
+        else
+        {
+            timerTime = pizza.counterTime;
         }
     }
 
     private void OnTriggerStay(Collider col)
     {
         //is the pizza in the oven and is it not burnt yet
-        if (col.transform.parent.TryGetComponent(out PizzaBehaviour pizza) && pizza.isBurnt == false)// && !gm.isPaused
+        if (!col.transform.parent.TryGetComponent(out PizzaBehaviour pizza) || pizza.isBurnt) return;
+        
+        pizza.cookedTime += Time.deltaTime; //adds time to the amount of time the pizza has been cooked
+        tempTime += Time.deltaTime; //time for fill on timer
+
+        timerTime -= Time.deltaTime; //time displayed on timer
+        string per = ((int)timerTime).ToString();
+
+        if (pizza.cookedTime > cookTime)
         {
-            pizza.cookedTime += Time.deltaTime; //adds time to the amount of time the pizza has been cooked
-            tempTime += Time.deltaTime; //time for fill on timer
+            loadingBar.fillAmount = tempTime / cookTime; //how much fill and assigns fill on timer
+        }
+        else
+        {
+            loadingBar.fillAmount = pizza.cookedTime / cookTime; //how much fill and assigns fill on timer
+        }
 
-            timerTime -= Time.deltaTime; //time displayed on timer
-            string per = ((int)timerTime).ToString();
+        progressIndicator.text = per;
 
-            if (pizza.cookedTime > cookTime)
+        //if it finished cooking this checks to see if it is going to burn/make it burnt
+        if (pizza.overCooking && pizza.cookedTime >= overCookTime)
+        {
+            pizza.GetComponentInChildren<MeshRenderer>().material = burnt;
+            pizza.isCooked = false;
+            pizza.isBurnt = true;
+            loadingBar.color = warnOrange;
+            PlayOvenAudio(OvenAudioStates.PizzaBurnt);
+            Invoke(nameof(BackToCookingSounds), 1f);
+        }
+
+        //else it is not fully cooked yet and it checks to see if it is cooked, and if it is starts the burn part of it
+        else if (pizza.cookedTime >= cookTime)
+        {
+            pizza.GetComponentInChildren<MeshRenderer>().material = cooked;
+            pizza.isCooked = true;
+            pizza.overCooking = true;
+            loadingBar.color = warnOrange;
+
+            if (changeOnce == false)
             {
-                loadingBar.fillAmount = tempTime / cookTime; //how much fill and assigns fill on timer
-            }
-
-            else
-            {
-                loadingBar.fillAmount = pizza.cookedTime / cookTime; //how much fill and assigns fill on timer
-            }
-
-            progressIndicator.text = per;
-            
-
-            //if (!audioSource.isPlaying)
-            //{
-            //    audioSource.clip = audioClips[0];
-            //    audioSource.loop = true;
-            //    audioSource.Play();
-            //}
-
-            //if it finished cooking this checks to see if it is going to burn/make it burnt
-            if (pizza.overCooking == true && pizza.cookedTime >= overCookTime)
-            {
-                pizza.GetComponentInChildren<MeshRenderer>().material = burnt;
-                pizza.isCooked = false;
-                pizza.isBurnt = true;
-                loadingBar.color = warnOrange;
-                //progressIndicator.text = "Burnt";
-
-                //audioSource.Stop();
-                //audioSource.clip = audioClips[1];
-                //audioSource.loop = false;
-                //audioSource.Play();
-
-                //audioSource.clip = audioClips[2];
-                //audioSource.Play();
-            }
-
-            //else it is not fully cooked yet and it checks to see if it is cooked, and if it is starts the burn part of it
-            else if (pizza.cookedTime >= cookTime)
-            {
-                col.gameObject.GetComponentInChildren<MeshRenderer>().material = cooked;
-                pizza.isCooked = true;
-                pizza.overCooking = true;
-                loadingBar.color = warnOrange;
-
-                if (changeOnce == false)
-                {
-                    changeOnce = true;
-                    tempTime = 0;
-                    timerTime = cookTime + 1;
-                    Debug.Log(timerTime);
-                }
-
-                //audioSource.Stop();
-                //audioSource.clip = audioClips[1];
-                //audioSource.loop = false;
-                //audioSource.Play();
+                PlayOvenAudio(OvenAudioStates.PizzaDone);
+                Invoke(nameof(BackToCookingSounds), 1f);
+                changeOnce = true;
+                tempTime = 0;
+                timerTime = cookTime + 1;
+                Debug.Log(timerTime);
             }
         }
     }
@@ -162,20 +120,42 @@ public class OvenBehaviour : MonoBehaviour
     private void OnTriggerExit(Collider col)
     {
         //if the pizza is removed reset all variables
-        if (col.gameObject.GetComponentInParent<PizzaBehaviour>())//&& !gm.isPaused
-        {
-            //ps.Stop();
-            //audioSource.Stop();
-            //inOven = false;
-            //currentTime = 0;
+        if (!col.transform.parent.TryGetComponent(out PizzaBehaviour pizza)) return;
+        ps.Stop();
+        audioSource.Stop();
 
-            //reset the timer when pizza leaves
-            loadingBar.color = goodGreen;
-            loadingBar.fillAmount = 0;
-            tempTime = 0;
-            col.gameObject.GetComponentInParent<PizzaBehaviour>().counterTime = timerTime;
-            timerTime = cookTime + 1;
-            
+        //reset the timer when pizza leaves
+        loadingBar.color = goodGreen;
+        loadingBar.fillAmount = 0;
+        tempTime = 0;
+        pizza.counterTime = timerTime;
+        timerTime = cookTime + 1;
+    }
+
+    private void BackToCookingSounds()
+    {
+        PlayOvenAudio(OvenAudioStates.Cooking);
+    }
+
+    private void PlayOvenAudio(OvenAudioStates state)
+    {
+        switch (state)
+        {
+            case OvenAudioStates.Cooking:
+                audioSource.clip = ovenAudioClips[(int) OvenAudioStates.Cooking];
+                audioSource.loop = true;
+                audioSource.Play();
+                break;
+            case OvenAudioStates.PizzaDone:
+                audioSource.clip = ovenAudioClips[(int) OvenAudioStates.PizzaDone];
+                audioSource.loop = false;
+                audioSource.Play();
+                break;
+            case OvenAudioStates.PizzaBurnt:
+                audioSource.clip = ovenAudioClips[(int) OvenAudioStates.PizzaBurnt];
+                audioSource.loop = false;
+                audioSource.Play();
+                break;
         }
     }
 }
