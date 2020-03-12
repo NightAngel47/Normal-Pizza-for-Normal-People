498c4176439d803963b216ff3a44c86869e39c20
sidfoe <sidfoe2125@gmail.com>
2020-03-11 19:38:21 -0500

    upgrade purchase

---
+++ Normal Pizza for Normal People/Assets/Scripts/UpradeItems/PurchColScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class PurchColScript : MonoBehaviour
+{
+    private ItemUpgrades iu;
+
+    private void Start()
+    {
+        iu = gameObject.GetComponentInParent<ItemUpgrades>();
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.GetComponentInParent<HandCollider>())
+        {
+            iu.Purchase();
+        }
+    }
+}
