a4b3247048d3307f57e3be03abd796eba757be88
Steven <NightAngel47@users.noreply.github.com>
2020-04-14 14:31:25 -0500

    Revert "Customer Line Overhaul"
    This reverts commit 6e4c70aea448d0a0e6b88acbea89b4a7fd118b43, reversing
    changes made to f9a140118fcc4ae16c150eeeee4f597b7f06f2da.

--- Normal Pizza for Normal People/Assets/Scripts/CustomerLine.cs
+++ Normal Pizza for Normal People/Assets/Scripts/CustomerLine.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.AI;
@@ -21,7 +20,7 @@ public class CustomerLine : MonoBehaviour
     [SerializeField]
     private Transform customerSpawnPos;
     [SerializeField]
-    private List<CustomerLinePos> customerLines = new List<CustomerLinePos>();
+    private List<Transform> customerLines = new List<Transform>();
 
     void Start()
     {
@@ -48,15 +47,9 @@ public class CustomerLine : MonoBehaviour
             
         var newCustomer = Instantiate(customerPrefab, customerSpawnPos.position, customerSpawnPos.rotation).GetComponent<Customer>();
         newCustomer.SetOrder(customerOrders[0]);
+        newCustomer.SetTargetLine(customerLines[0].position);
+        newCustomer.GetComponent<NavMeshAgent>().SetDestination(customerLines[0].position);
         customerOrders.Remove(customerOrders[0]);
-
-        foreach (var line in customerLines.Where(line => line.isOpen))
-        {
-            line.isOpen = false;
-            newCustomer.SetTargetLine(customerLines[customerLines.IndexOf(line)].transform.position);
-            newCustomer.GetComponent<NavMeshAgent>().SetDestination(customerLines[customerLines.IndexOf(line)].transform.position);
-            break;
-        }
         
         yield return new WaitForSeconds(gameManager.currentGameDay.dayLength / gameManager.currentGameDay.numOfCustomers);
         
