65596fdffcef56f4509feb04ad71cc6732113176
sidfoe <sidfoe2125@gmail.com>
2020-04-16 15:11:06 -0400

    hehe hoho haha

--- Normal Pizza for Normal People/Assets/Scripts/Interactable Objects/IngredientHitEffect.cs
+++ Normal Pizza for Normal People/Assets/Scripts/Interactable Objects/IngredientHitEffect.cs
@@ -25,6 +25,6 @@ public class IngredientHitEffect : MonoBehaviour
             pizza.AddPizzaIngredient(newIngredient.GetComponent<PizzaIngredient>());
         }
 
-        Destroy(gameObject);
+        gameObject.SetActive(false);
     }
 }
