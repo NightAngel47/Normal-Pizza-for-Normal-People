356e34a09af804e1dca32bfae40776f9ec2b2570
Steven <NightAngel47@users.noreply.github.com>
2020-04-23 11:51:18 -0500

    Made New Topping and Line Appear with Upgrades

--- Normal Pizza for Normal People/Assets/Scripts/UpradeItems/ItemUpgrades.cs
+++ Normal Pizza for Normal People/Assets/Scripts/UpradeItems/ItemUpgrades.cs
@@ -4,30 +4,11 @@ using UnityEngine;
 
 public abstract class ItemUpgrades : MonoBehaviour
 {
-    //public Material blueprintMaterial; //material for when it is not avaiable for purchase
-    //public Material activeMaterial; //material for when it is purchased its 'normal' material
-
-    //private MoneyTracker mt; //reference to moneytracker so that it can purchase the upgrades and subtract from total money amount
-
-    //public int itemCost; //cost of upgrade. set individually by each upgrade
-
-    //public OrderCreation oc;
-    //public bool isTopping;
-
-    
-
-    private void Start()
-    {
-        //finds money tracker ref
-        //mt = FindObjectOfType<MoneyTracker>();
-        //oc = FindObjectOfType<OrderCreation>();
-    }
 
     //displays items with the available for purchase material during ugrade period
     public void ShowItem()
     {
         gameObject.SetActive(true);
-        //ChangeMaterial(blueprintMaterial);
     }
 
     //hides items when upgrade period is done
@@ -46,29 +27,8 @@ public abstract class ItemUpgrades : MonoBehaviour
         gameObject.GetComponent<AudioSource>().Play();
         Destroy(gameObject.GetComponent<ItemUpgrades>()); // Destroy item upgrade so it is not readded to upgrade system
 
-        //PLAYER CHOOSE UPGRADE LEFTOVER CODE
-        //if (mt.Purchase(itemCost)) //do we have enough money for the upgrade
-        //{
-        //    gameObject.SetActive(true); //make sure it is active, probably do not need this line
-        //    TurnOffPurchaseCollider(); //turns off purchase collider(which is what detects when the player wants to buy) and its timer as well
-        //    TurnOnUpgrade(); //makes sure upgrade funcitonality works 
-        //    ChangeMaterial(activeMaterial); //changes it to the active normal material
-        //    FindObjectOfType<UpgradeSystem>().RemovedPurchasedUpgrade(gameObject); // Remove this gameobject from upgrade system
-        //    Destroy(gameObject.GetComponent<ItemUpgrades>()); // Destroy item upgrade so it is not readded to upgrade system
-        //}
-
-        //else //not enonugh money
-        //{
-        //    Debug.Log("Cannot Buy Not Enough Mons");
-        //}
     }
 
     //toggles object functionality
     public abstract void TurnOnUpgrade();
-
-    //Turns off the purchase collider and the timer for confirming the purchase
-    protected abstract void TurnOffPurchaseCollider();
-
-    //updates the material of the upgrade
-    protected abstract void ChangeMaterial(Material changeMat);
 }
