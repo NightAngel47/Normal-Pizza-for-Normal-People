c7034de592f58e1e3c797a705d9a5c4e261a000b
Steven <NightAngel47@users.noreply.github.com>
2020-04-23 10:50:06 -0500

    Seperated Tutorial UI

--- Normal Pizza for Normal People/Assets/Scripts/TutorialUIHandler.cs
+++ Normal Pizza for Normal People/Assets/Scripts/TutorialUIHandler.cs
@@ -6,47 +6,45 @@ public class TutorialUIHandler : MonoBehaviour
 {
     public List<GameObject> tutorialUI = new List<GameObject>();
     private GameManager gm;
-    private bool runThroughOnce = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        gm = GetComponent<GameManager>();
+        
         for(int i = 0; i < tutorialUI.Count; i++)
         {
             tutorialUI[i].SetActive(false);
         }
 
-        gm = GetComponent<GameManager>();
-        tutorialUI[0].SetActive(true); //start text it up
+        StartCoroutine(RunTutorial());
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Runs through the tutorial and waits for events to happen before continuing
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator RunTutorial()
     {
-        if (runThroughOnce)
-        {
-            if (gm.dayStarted == true)
-            {
-                tutorialUI[0].SetActive(false); //start off
-                tutorialUI[1].SetActive(true); //smack on
-            }
-            if (FindObjectOfType<PizzaBehaviour>())
-            {
-                tutorialUI[1].SetActive(false); //smack off
-                tutorialUI[2].SetActive(true); //grab on
-            }
-
-            if (FindObjectOfType<IngredientHitEffect>())
-            {
-                tutorialUI[2].SetActive(false); //grab off
-                tutorialUI[3].SetActive(true); //throw on
-            }
-
-            if (Customer.firstPizzaThrow == true)
-            {
-                tutorialUI[3].SetActive(false); //throw off
-                runThroughOnce = true;
-            }
-        }
+        tutorialUI[0].SetActive(true); //start text it up
+        
+        yield return new WaitUntil(() => gm.dayStarted);
+        
+        tutorialUI[0].SetActive(false); //start off
+        tutorialUI[1].SetActive(true); //smack on
+        
+        yield return new WaitUntil(() => FindObjectOfType<PizzaBehaviour>());
+        
+        tutorialUI[1].SetActive(false); //smack off
+        tutorialUI[2].SetActive(true); //grab on
+        
+        yield return new WaitUntil(() => FindObjectOfType<IngredientHitEffect>());
+        
+        tutorialUI[2].SetActive(false); //grab off
+        tutorialUI[3].SetActive(true); //throw on
+        
+        yield return new WaitUntil(() => Customer.firstPizzaThrow);
+        
+        tutorialUI[3].SetActive(false); //throw off
     }
 }
