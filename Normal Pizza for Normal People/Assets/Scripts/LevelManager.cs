da417464f666c9db91ca7463b943a3723e61c20e
sidfoe <sidfoe2125@gmail.com>
2020-10-06 13:20:49 -0500

    push

--- Normal Pizza for Normal People/Assets/Scripts/LevelManager.cs
+++ Normal Pizza for Normal People/Assets/Scripts/LevelManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
+    public Sheet1 levelData;
+    private int currentDay = 0;
+
     // Start is called before the first frame update
     void Start()
     {
