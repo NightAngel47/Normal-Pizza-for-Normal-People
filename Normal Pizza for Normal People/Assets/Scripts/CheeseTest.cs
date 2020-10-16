93c25b9ad1ee248cfc37ec9d727d2fec3c5dad02
sidfoe <sidfoe2125@gmail.com>
2020-10-16 13:51:34 -0500

    cheese machine working

--- Normal Pizza for Normal People/Assets/Scripts/CheeseTest.cs
+++ Normal Pizza for Normal People/Assets/Scripts/CheeseTest.cs
@@ -1,18 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR.InteractionSystem;
 
 public class CheeseTest : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject cheese;
+    public GameObject linear;
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider col)
     {
-        
+        if (col.transform.parent.TryGetComponent(out PizzaBehaviour pizza))
+        { 
+            Debug.Log(linear.GetComponent<LinearMapping>().value);
+            if (linear.GetComponent<LinearMapping>().value >= .7f)
+            {
+                Debug.Log("here2");
+                var spawnPos = pizza.transform.position;
+                var newIngredient = Instantiate(cheese, spawnPos, pizza.transform.rotation, pizza.transform);
+                newIngredient.transform.Rotate(Vector3.up, Random.Range(-180, 180), Space.Self);
+                pizza.AddPizzaIngredient(newIngredient.GetComponent<PizzaIngredient>());
+            }
+        }
     }
 }
