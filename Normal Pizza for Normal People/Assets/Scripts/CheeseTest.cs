c3c92949fce702877e59c4676494b2524d13cb1b
sidfoe <sidfoe2125@gmail.com>
2020-10-16 13:52:04 -0500

    got rid of some debug lines

--- Normal Pizza for Normal People/Assets/Scripts/CheeseTest.cs
+++ Normal Pizza for Normal People/Assets/Scripts/CheeseTest.cs
@@ -12,10 +12,8 @@ public class CheeseTest : MonoBehaviour
     {
         if (col.transform.parent.TryGetComponent(out PizzaBehaviour pizza))
         { 
-            Debug.Log(linear.GetComponent<LinearMapping>().value);
             if (linear.GetComponent<LinearMapping>().value >= .7f)
             {
-                Debug.Log("here2");
                 var spawnPos = pizza.transform.position;
                 var newIngredient = Instantiate(cheese, spawnPos, pizza.transform.rotation, pizza.transform);
                 newIngredient.transform.Rotate(Vector3.up, Random.Range(-180, 180), Space.Self);
