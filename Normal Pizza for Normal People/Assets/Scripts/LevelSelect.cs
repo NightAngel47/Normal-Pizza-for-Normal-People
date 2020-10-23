8cd2c6975c8692aded9c331fc3ab0b923a1ee839
Steven <NightAngel47@users.noreply.github.com>
2020-10-23 16:06:34 -0500

    Made Level Select just static

--- Normal Pizza for Normal People/Assets/Scripts/LevelSelect.cs
+++ Normal Pizza for Normal People/Assets/Scripts/LevelSelect.cs
@@ -1,23 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class LevelSelect : MonoBehaviour
+﻿
+public class LevelSelect
 {
     public static int selectedLevel;
-
-    public static LevelSelect instance;
-
-    public void Awake()
-    {
-        if(instance == null)
-        {
-            instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
-    }
 }
