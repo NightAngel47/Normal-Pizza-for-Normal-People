d7e0c861069aa8443cf59529c5189ca5ae90fcfb
Fisher Heins <fisher@heinsdesign.com>
2020-10-23 14:37:48 -0500

    Merge branch 'master' of https://github.com/NightAngel47/Normal-Pizza-for-Normal-People

---
+++ Normal Pizza for Normal People/Assets/Scripts/HatRandomizer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Randomzies what hat a customer wears
+public class HatRandomizer : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] hats;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int hat = Random.Range(0, hats.Length+1);
+        if(hat < hats.Length)
+        {
+            hats[hat].SetActive(true);
+        }
+    }
+}
