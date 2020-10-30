7ff8002ecc176e258050dfa9683ad44fec0c75ad
Steven <NightAngel47@users.noreply.github.com>
2020-10-30 15:20:32 -0500

    ha

--- Normal Pizza for Normal People/Assets/Scripts/CustomerLinePos.cs
+++ Normal Pizza for Normal People/Assets/Scripts/CustomerLinePos.cs
@@ -6,11 +6,13 @@
  * Handles when customer lines are available
  */
 
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CustomerLinePos : MonoBehaviour
 {
     public bool isOpen = true;
+    public int customersInLine = 0;
 
     private void OnTriggerExit(Collider other)
     {
